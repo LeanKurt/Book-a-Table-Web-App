7a51c7e6a842e1ab9525ce4d7c0b44df109bf5a5
Lean Kurt <leankurtescano@gmail.com>
2024-03-12 20:42:42 +0800

    added api

--- src/sections/Reservation.jsx
+++ src/sections/Reservation.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../registration.css'
 import { glass, people, time } from '../assets/images';
 
+
 const Reservation = () => {
+
+ 
   const [toggle, setToggle] = useState(false);
-  const [ toggle2, setToggle2] = useState(false);
-  const [ toggle3, setToggle3] = useState(false);
+  const [toggle2, setToggle2] = useState(false);
+  const [toggle3, setToggle3] = useState(false);
   const [occasion, setOccasion] = useState("Occasion");
   const [setting, setSetting] = useState("")
   const [date, setDate] = useState('');
   const [peoples, setPeoples] = useState("No. of Diners");
-  const [ times, setTimes] = useState("Select Time");
+  const [times, setTimes] = useState("Select Time");
   const [fill, setFill] = useState(false);
   const [fill2, setFill2] = useState(false);
   const [fill3, setFill3] = useState(false);
   const [fill4, setFill4] = useState(false);
-  const [submit , setSubmit] = useState(true);
-    const handleDate = (event) => {
+  const [submit, setSubmit] = useState(true);
+
+
+  useEffect(() => {
+       
+  }, [])
+  const handleDate = (event) => {
     setDate(event.target.value);
     setFill4(true);
   }
- 
+
   const handleRadio = (event) => {
-       setSetting(event.target.value);
+    setSetting(event.target.value);
   }
   const toggleButton = () => {
     setToggle(!toggle);
@@ -44,9 +52,9 @@ const Reservation = () => {
   };
 
   const handlePeople = (value) => {
-     setPeoples(value)
-     setToggle2(false)
-     setFill2(true)
+    setPeoples(value)
+    setToggle2(false)
+    setFill2(true)
   };
 
   const handleTime = (value) => {
@@ -55,17 +63,18 @@ const Reservation = () => {
     setFill3(true);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    alert("Reservation completed");
   }
 
 
   const ErrorMessage = () => {
-    return(
+    return (
       <div>
         <p className='error'> *Please fill in all required field</p>
       </div>
-   
+
     )
   }
 
@@ -78,108 +87,102 @@ const Reservation = () => {
   return (
     <section className='registration-form'>
       <form onSubmit={handleSubmit}>
-      <div className='registration-container'>
-        <div className="reservation">
-          <div className="heading">
-            <h1>Registration</h1>
-          </div>
-          <div className='indoor'>
-            <label htmlFor='indoor'>Indoor Seating</label>
-            <input type='radio' value="Indoor" onChange={handleRadio}  checked={setting === "Indoor"} disabled={setting === "Outdoor"}id='indoor'  name='indoor'/>
-          </div>
-          <div className='date-container'>
-            <label htmlFor="date">Date</label>
-            <input type='date' name='date' id='date' onChange={handleDate} className={`dateset ${fill4 ? "filled" : ""}`}></input>
-          </div>
-          <div className="reservation-container">
-            <p className='occa'>Occasion</p>
-          <div onClick={toggleButton} className={`button-container ${fill ? "filled" : ""}`}>
-            <div className="formlogo-container">
-              <img src={glass} className='form-logo' alt="Glass" />
+        <div className='registration-container'>
+          <div className="reservation">
+            <div className="heading">
+              <h1>Reservation</h1>
             </div>
-            <div className='occassion'>
-              <p className={fill ? "white" : "normal"}>{occasion}</p>
+            <div className='indoor'>
+              <label htmlFor='indoor'>Indoor Seating</label>
+              <input type='radio' value="Indoor" onChange={handleRadio} checked={setting === "Indoor"} disabled={setting === "Outdoor"} id='indoor' name='indoor' />
             </div>
-            <div className='chevron'><p>{toggle ? "^" : "v"}</p></div>
+            <div className='date-container'>
+              <label htmlFor="date">Date</label>
+              <input type='date' name='date' id='date' onChange={handleDate} className={`dateset ${fill4 ? "filled" : ""}`}></input>
+            </div>
+            <div className="reservation-container">
+              <p className='occa'>Occasion</p>
+              <div onClick={toggleButton} className={`button-container ${fill ? "filled" : ""}`}>
+                <div className="formlogo-container">
+                  <img src={glass} className='form-logo' alt="Glass" />
+                </div>
+                <div className='occassion'>
+                  <p className={fill ? "white" : "normal"}>{occasion}</p>
+                </div>
+                <div className='chevron'><p>{toggle ? "^" : "v"}</p></div>
+              </div>
+              <div className={`option-container ${toggle ? "open" : ""}`}>
+                <div className='options' onClick={() => handleOption("Birthday")}><p>Birthday</p></div>
+                <div className='options' onClick={() => handleOption("Anniversary")}><p>Anniversary</p></div>
+                <div className='options' onClick={() => handleOption("Wedding")}><p>Wedding</p></div>
+              </div>
+            </div>
           </div>
-          
-          <div className={`option-container ${toggle ? "open" : ""}`}>
-            <div className='options' onClick={() => handleOption("Birthday")}><p>Birthday</p></div>
-            <div className='options' onClick={() => handleOption("Anniversary")}><p>Anniversary</p></div>
-            <div className='options' onClick={() => handleOption("Wedding")}><p>Wedding</p></div>
-          </div>
+          <div className="reservation2">
+            <div className='outdoor'>
+              <label htmlFor='Outdoor'>Outdoor Seating</label>
+              <input type='radio' id='outdoor' name='outdoor' value="Outdoor" checked={setting === "Outdoor"} disabled={setting === "Indoor"} onChange={handleOption} />
+            </div>
+            <div className='reservation-container'>
+              <p>Number of Diners</p>
+              <div onClick={toggleButton2} className={`button-container2 ${fill2 ? "filled" : ""}`}>
+                <div className="formlogo-container">
+                  <img src={people} className='form-logo' alt="Glass" />
+                </div>
+                <div className='occassion'>
+                  <p className={fill2 ? "white" : "normal"}>{peoples}</p>
+                </div>
+                <div className='chevron'><p>{toggle2 ? "^" : "v"}</p></div>
+              </div>
+              <div className={`column-container ${toggle2 ? "open" : ""}`}>
+                <div className='column1'>
+                  <div onClick={() => handlePeople("1 Diner")} className='people-option'><p>1 Diner</p></div>
+                  <div onClick={() => handlePeople("3 Diner")} className='people-option'><p>3 Diner</p></div>
+                  <div onClick={() => handlePeople("5 Diner")} className='people-option'><p>5 Diner</p></div>
+                  <div onClick={() => handlePeople("7 Diner")} className='people-option'><p>7 Diner</p></div>
+                  <div onClick={() => handlePeople("9 Diner")} className='people-option'><p>9 Diner</p></div>
+                </div>
+                <div className="column2">
+                  <div onClick={() => handlePeople("2 Diner")} className='people-option'><p>2 Diner</p></div>
+                  <div onClick={() => handlePeople("4 Diner")} className='people-option'><p>4 Diner</p></div>
+                  <div onClick={() => handlePeople("6 Diner")} className='people-option'><p>6 Diner</p></div>
+                  <div onClick={() => handlePeople("8 Diner")} className='people-option'><p>8 Diner</p></div>
+                  <div onClick={() => handlePeople("10 Diner")} className='people-option'><p>10 Diner</p></div>
+                </div>
+              </div>
+            </div>
+            <div className='reservation-container'>
+              <p>Time</p>
+              <div onClick={toggleButton3} className={`button-container3 ${fill3 ? "filled" : ""}`}>
+                <div className="formlogo-container">
+                  <img src={time} className='form-logo' alt="Glass" />
+                </div>
+                <div className='occassion'>
+                  <p className={fill3 ? "white" : "normal"}>{times}</p>
+                </div>
+                <div className='chevron'><p>{toggle3 ? "^" : "v"}</p></div>
+              </div>
+              <div className={`column-container ${toggle3 ? "open" : ""}`}>
+                <div className='column1'>
+                  <div onClick={() => handleTime("5:00 pm")} className="time-option">5:00 pm</div>
+                  <div onClick={() => handleTime("7:00 pm")} className="time-option">7:00 pm</div>
+                  <div onClick={() => handleTime("9:00 pm")} className="time-option">9:00 pm</div>
+                </div>
+                <div className='column2'>
+                  <div onClick={() => handleTime("6:00 pm")} className="time-option">6:00 pm</div>
+                  <div onClick={() => handleTime("8:00 pm")} className="time-option">8:00 pm</div>
+                  <div onClick={() => handleTime("10:00 pm")} className="time-option">10:00 pm</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="reservation2">
-        <div className='outdoor'>
-            <label htmlFor='Outdoor'>Outdoor Seating</label>
-            <input type='radio' id='outdoor' name='outdoor'   value = "Outdoor" checked={setting === "Outdoor"} disabled ={setting === "Indoor"}onChange={handleOption}/>
-          </div>
-          <div className='reservation-container'>
-          <p>Number of Diners</p>
-        <div onClick={toggleButton2} className={`button-container2 ${fill2 ? "filled" : ""}`}>
-            <div className="formlogo-container">
-              <img src={people} className='form-logo' alt="Glass" />
-            </div>
-            <div className='occassion'>
-              <p className={fill2 ? "white" : "normal"}>{peoples}</p>
-            </div>
-            <div className='chevron'><p>{toggle2 ? "^" : "v"}</p></div>
-          </div>
-          <div className={`column-container ${toggle2 ? "open" : ""}`}>
-             <div className='column1'>
-               <div onClick={() => handlePeople("1 Diner")}className='people-option'><p>1 Diner</p></div>
-               <div onClick={() => handlePeople("3 Diner")}className='people-option'><p>3 Diner</p></div>
-               <div onClick={() => handlePeople("5 Diner")}className='people-option'><p>5 Diner</p></div>
-               <div onClick={() => handlePeople("7 Diner")}className='people-option'><p>7 Diner</p></div>
-               <div onClick={() => handlePeople("9 Diner")}className='people-option'><p>9 Diner</p></div>
-               
-                
-             </div>
-             <div className="column2">
-             <div onClick={() => handlePeople("2 Diner")}className='people-option'><p>2 Diner</p></div>
-               <div onClick={() => handlePeople("4 Diner")}className='people-option'><p>4 Diner</p></div>
-               <div onClick={() => handlePeople("6 Diner")}className='people-option'><p>6 Diner</p></div>
-               <div onClick={() => handlePeople("8 Diner")}className='people-option'><p>8 Diner</p></div>
-               <div onClick={() => handlePeople("10 Diner")}className='people-option'><p>10 Diner</p></div>
- 
-             </div>
-          </div>
-          </div>
-
-          <div className='reservation-container'>
-          <p>Time</p>
-          <div onClick={toggleButton3} className={`button-container3 ${fill3 ? "filled" : ""}`}>
-            <div className="formlogo-container">
-              <img src={time} className='form-logo' alt="Glass" />
-            </div>
-            <div className='occassion'>
-              <p className={fill3 ? "white" : "normal"}>{times}</p>
-            </div>
-            <div className='chevron'><p>{toggle3 ? "^" : "v"}</p></div>
-          </div>
-          <div className={`column-container ${toggle3 ? "open" : ""}`}>
-            <div className='column1'>
-              <div onClick={() => handleTime("5:00 pm")}className="time-option">5:00 pm</div>
-              <div onClick={() => handleTime("7:00 pm")}className="time-option">7:00 pm</div>
-              <div onClick={() => handleTime("9:00 pm")}className="time-option">9:00 pm</div>
-            </div>
-            <div className='column2'>
-            <div onClick={() => handleTime("6:00 pm")}className="time-option">6:00 pm</div>
-              <div onClick={() => handleTime("8:00 pm")}className="time-option">8:00 pm</div>
-              <div onClick={() => handleTime("10:00 pm")}className="time-option">10:00 pm</div>
-            </div>
-          </div>
-          </div>
+        <div className='submit-container'>
+          <button type="submit" className='booking-button' disabled={!validateInput()}>Reserve a Table</button>
+          {!validateInput() ? (
+            <ErrorMessage />
+          ) : null}
         </div>
-      </div>
-      <div className='submit-container'>
-        <button className='booking-button' disabled={!validateInput()}>Reserve a Table</button> 
-        {!validateInput() ? (
-            <ErrorMessage /> 
-           ) : null}
-      </div>
-      
       </form>
     </section>
   )
