425ba243ccd939873de4fd7435d80871ac2ef334
Lean Kurt <leankurtescano@gmail.com>
2024-03-19 09:57:52 +0800

    made booking-details

--- src/sections/MyBooking.jsx
+++ src/sections/MyBooking.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MyContext } from './MyContext';
 import { useContext } from 'react';
+import '../Mybooking.css'
 
 
 const MyBooking = () => {
@@ -16,22 +17,32 @@ const MyBooking = () => {
     return (
         <section className='confirmed'>
             <div className='card-container'>
+                <div className="booking-date">
+                    <h1>Your Booking was made on {dateToday}</h1>
+                </div>
+                <div className="container-all">
                <div className='container-1'>
                 <div className="setting-container">
+                    <p>Date : {date}</p>
+                    <p>Setting: {setting}</p>
 
                 </div>
                 <div className="occasion-container">
+                    <p>Occassion: {occasion}</p>
 
                 </div>
 
                </div>
                <div className="container-2">
                 <div className="no-people">
+                    <p>No. of people : {peoples}</p>
 
                 </div>
                 <div className="time-container">
-                    
+                    <p>Time : {times}</p>
+
                 </div>
+               </div>
                </div>
 
             </div>
